e6fb7543d773a13baaaa5d1ad4cc653de6a33afd
Nahom Bekele <nahom.fix@gmail.com>
2022-05-21 16:22:28 +0300

    fix: relative file path fix

--- scripts/config.py
+++ scripts/config.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 
 class Config:
-  RANDOM_SEED = 42
-  ASSETS_PATH = Path("../")
-  REPO = "/Users/samrit/Documents/10Academy/week2/SmartAd contribution/abtest-ml/abtest-mlops"
-  DATASET_FILE_PATH = "data/AdSmartABdata.csv"
-  DATASET_PATH = ASSETS_PATH / "data"
-  FEATURES_PATH = ASSETS_PATH / "features"
-  MODELS_PATH = ASSETS_PATH / "models"
-  METRICS_FILE_PATH = ASSETS_PATH / "metrics"
+    ROOT_DIR = Path.cwd()
+    RANDOM_SEED = 42
+    ASSETS_PATH = Path("../")
+    REPO = "/Users/samrit/Documents/10Academy/week2/SmartAd contribution/abtest-ml/abtest-mlops"
+    DATASET_FILE_PATH = "data/AdSmartABdata.csv"
+    DATASET_PATH = ROOT_DIR / "data"
+    FEATURES_PATH = ROOT_DIR / "features"
+    MODELS_PATH = ROOT_DIR / "models"
+    METRICS_FILE_PATH = ROOT_DIR / "metrics"
